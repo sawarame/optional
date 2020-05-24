59899f347c3297763634a4739e73d7b3acf0693a
Yasushi Sawarame <yasushi@sawara.me>
2020-05-24 11:49:37 +0900

    add functions.

--- src/Optional.php
+++ src/Optional.php
@@ -2,8 +2,10 @@
 
 namespace Sawarame;
 
+use Prophecy\Promise\ThrowPromise;
 use Sawarame\Optional\Exception\NullPointerException;
 use Sawarame\Optional\Exception\NoSuchElementException;
+use Throwable;
 
 class Optional
 {
@@ -45,6 +47,34 @@ class Optional
         return ! is_null($this->value);
     }
 
+    public function isEmpty(): bool
+    {
+        return is_null($this->value);
+    }
+
+    public function ifPresent(callable $action): void
+    {
+        if (! is_null($this->value)) {
+            $action($this->value);
+        }
+    }
+
+    public function ifPresentOrElse(callable $action, callable $emptyAction): void
+    {
+        $this->ifPresent($action);
+        if (is_null($this->value)) {
+            $emptyAction();
+        }
+    }
+
+    public function filter(callable $predicate): self
+    {
+        if ($predicate($this->value)) {
+            return self::ofNullable($this->value);
+        }
+        return self::empty();
+    }
+
     public function map(callable $mapper): self
     {
         if (! is_null($this->value)) {
@@ -52,5 +82,49 @@ class Optional
             return new self($value);
         }
         return self::empty();
+    }
+
+    public function flatMap(callable $mapper)
+    {
+        if (! is_null($this->value)) {
+            return $this->map($mapper)->get();
+        }
+        return self::empty();
+    }
+
+    public function or(callable $supplier): self
+    {
+        if (! is_null($this->value)) {
+            return self::of($this->value);
+        }
+        return $supplier();
+    }
+
+    public function orElse($other)
+    {
+        if (! is_null($this->value)) {
+            return $this->get();
+        }
+        return $other;
+    }
+
+    public function orElseGet(callable $supplier)
+    {
+        if (! is_null($this->value)) {
+            return $this->get();
+        }
+        return $supplier();
+    }
+
+    public function orElseThrow(?callable $exceptionSupplier = null)
+    {
+        if (is_null($exceptionSupplier)) {
+            throw new NoSuchElementException();
+        }
+        $exception = $exceptionSupplier();
+        if (is_a($exception, 'Exception', true) || is_a($exception, 'Throwable', true)) {
+            throw $exception;
+        }
+        throw new NoSuchElementException();
     }
 }
